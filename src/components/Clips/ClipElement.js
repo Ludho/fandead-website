f23e77a3a8e8991c395668cf7a0cfcd4c543362d
Ludho <Ludho234@gmail.com>
2022-01-20 01:15:06 +0100

    Modify ClipElement
    Add youtube  link+img

--- src/components/Clips/ClipElement.js
+++ src/components/Clips/ClipElement.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 const ClipElement = ({ data }) =>  {
-    var vtuberArr = data.vtuber.split(',');
-    var imgref= 'https://i.ytimg.com/vi_webp/'+data.id+'/mqdefault.webp'
+    let vtuberArr = data.vtuber.split(',');
+    let imgref= 'https://i.ytimg.com/vi_webp/'+data.id+'/mqdefault.webp';
+    let ytref='https://www.youtube.com/watch?v='+data.id;
     return (
         <>
         <div class="col-md-3">
-            <a>
-              <img href={imgref}/>
+            <a href={ytref} 
+                style={{
+                    display: 'block',
+                    height: '200px'
+                }}
+            >
+                <img src={imgref} class="img-fluid"
+                                style={{
+                    height: '200px',
+                    width:'306px'
+                }}
+                />
             </a>
             <h3>{data.title}</h3>
             <p>{data.uploadTime}</p>
